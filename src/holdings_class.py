07efeea19472f908c1f2171ed087d04ef9b0c57e
BernardFr <bfraenkel@gmail.com>
2026-01-10 17:17:47 -0800

    Working holdings_class

--- src/holdings_class.py
+++ src/holdings_class.py
@@ -25,17 +25,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
-
-import openpyxl
-
-                             
-import utilities as ut
+                            
+from utilities import error_exit
 from configuration_manager_class import ConfigurationManager
 from logger import logger
-
-MARKET_VALUE_SHEET = 'Market Value'
-MARKET_VALUE_COL = 'Market Value'
-DEBUG_FLAG = False
 
 class Holdings:
     """Main orchestrator class for Market Value processing.
@@ -51,8 +44,10 @@ class Holdings:
         self.cash_amount_string = self.config['Cash_Amount_String']
         self.cols_2_keep = self.config['Cols_2_Keep']
         self.detail_columns = self.config['Detail_Columns']
+        self.etf_asset_class_map_file = self.config['ETF_Asset_Class_Map_File']
 
-    def _load_all_transaction_data(self) -> tuple[pd.DataFrame, List[str], float]:
+
+    def load_holdings_data(self) -> tuple[pd.DataFrame, List[str], float]:
         """Load all transaction data from holdings file."""
         # Get the latest Holdings file from Holdings directory
         holdings_file, holdings_date = self._get_holdings_file()
@@ -65,7 +60,7 @@ class Holdings:
             f"{days_old}")
 
         # Load the data from the holdings file
-        holdings_df, cash_amount = self._load_holdings_data(holdings_file)
+        holdings_df, cash_amount = self._read_holdings_data(holdings_file)
         return holdings_df, cash_amount  # return holdings_df
 
     def _get_holdings_file(self) -> tuple[str, str]:
@@ -101,7 +96,7 @@ class Holdings:
          column"""
         return float(in_df[in_df.iloc[:, 0] == self.cash_amount_string].iloc[:, 2].values[0])
 
-    def _load_holdings_data(self, holdings_file: str) -> tuple[pd.DataFrame, float]:
+    def _read_holdings_data(self, holdings_file: str) -> tuple[pd.DataFrame, float]:
         """Load the holdings data from the file"""
         in_file = os.path.join(self.data_dir, holdings_file)
         # openpyxl does not work with .xls files - use xlrd instead
@@ -145,15 +140,75 @@ class Holdings:
         working_df = working_df.drop(columns=['Tax Term'])
         return working_df
 
+    def assign_cash_to_etf(self, in_df: pd.DataFrame, cash_amount: float) -> pd.DataFrame:
+        """ Assign cash to an ETF ... to keep it simple """
+        working_df = in_df.copy(deep=True)
+        # Find the row where the Symbol is ETF_for_cash
+        etf_for_cash_symbol = self.config['ETF_for_cash']
+        # Find if etf_for_cash_symbol is already part of the holdings
+        if etf_for_cash_symbol in working_df['Symbol'].values:
+            logger.info(f"Adding ${cash_amount:,.2f} to {etf_for_cash_symbol} - already part of the holdings")
+            etf_for_cash_row = working_df[working_df['Symbol'] == etf_for_cash_symbol]
+            # Reassign the cash to the ETF_for_cash column
+            working_df.loc[etf_for_cash_row.index, 'Market Value'] = cash_amount + working_df.loc[etf_for_cash_row.index, 'Market Value']
+        else:
+            # Add a new row for the ETF_for_cash symbol
+            logger.info(f"Adding new row {etf_for_cash_symbol} for cash: ${cash_amount:,.2f}")
+            new_row = pd.DataFrame({'Symbol': [etf_for_cash_symbol], 'Shares': [cash_amount], 'Market Value': [cash_amount]})
+            working_df = pd.concat([working_df, new_row], ignore_index=True)
+            # Sort the dataframe by Symbol
+            working_df = working_df.sort_values(by='Symbol')
+            working_df.reset_index(drop=True, inplace=True)
+        logger.info(f"Total Portfolio Market Value: ${working_df['Market Value'].sum():,.2f}")
+        return working_df
+
+    def set_holdings_df(self, in_df: pd.DataFrame) -> None:
+        """ Set the holdings dataframe """
+        self.holdings_df = in_df
+        return 
+
+
+    def map_etf_to_asset_class(self) -> pd.DataFrame:
+        """ Map the ETFs to the asset classes """
+        # read the mapping file ETF -> Asset Class
+        # Read into a dictionary - where ETF is the key and Asset Class is the value
+        etf_asset_class_df = pd.read_excel(self.etf_asset_class_map_file, sheet_name='ETF - Asset Class', dtype=str, engine='openpyxl')
+        # Create a mapping dictionary using the first column as key and second column as value
+        etf_asset_class_map_dict = etf_asset_class_df.set_index(etf_asset_class_df.columns[0])[etf_asset_class_df.columns[1]].to_dict()
+        
+        # map the ETFs to the asset classes - using the dictionary and assign "Not_Found" if the ETF is not found
+        self.holdings_df['Asset Class'] = self.holdings_df['Symbol'].map(etf_asset_class_map_dict).fillna('Not_Found')
+        # Create a list of ETFs that were not found
+        not_found_etfs = self.holdings_df[self.holdings_df['Asset Class'] == 'Not_Found']['Symbol'].values
+        if len(not_found_etfs) > 0:
+            error_exit(f"The following ETFs were not found in the mapping file: {not_found_etfs}")
+
+        # Create a new DF with asset class and Market Value by aggregating the Market Value by asset class
+        asset_class_df = self.holdings_df.groupby('Asset Class')['Market Value'].sum().reset_index()
+        # Sort the dataframe by Asset Class
+        asset_class_df = asset_class_df.sort_values(by='Asset Class')
+        asset_class_df.reset_index(drop=True, inplace=True)
+        self.portfolio_assets_df = asset_class_df
+        logger.info(f"Total Portfolio Market Value: ${asset_class_df['Market Value'].sum():,.2f}")
+        return asset_class_df
+
+    def get_total_portfolio_market_value(self) -> float:
+        """ Get the total portfolio market value """
+        return self.portfolio_assets_df['Market Value'].sum()
 
 def main(cmd_line: List[str]):
     """Main entry point for BenPlan.
     processor_kill_flag: if True, async processes will be killed by the cleanup function
     """
     portfolio = Holdings(cmd_line)
-    holdings_df, cash_amount = portfolio._load_all_transaction_data()
+    holdings_df, cash_amount = portfolio.load_holdings_data()
     print(f"Holdings DataFrame:\n{holdings_df}")
     print(f"Cash amount: ${cash_amount:,.2f}")
+    holdings_df = portfolio.assign_cash_to_etf(holdings_df, cash_amount)
+    print(f"Holdings DataFrame after reassigning cash to ETF_for_cash:\n{holdings_df}")
+    portfolio.set_holdings_df(holdings_df)
+    asset_class_df = portfolio.map_etf_to_asset_class()
+    print(f"Asset Class DataFrame:\n{asset_class_df}")
     return
 
 
